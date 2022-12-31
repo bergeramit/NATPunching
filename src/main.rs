fac548faca331b30805cba487bc3d8c19398246a
Amit Berger <bergeramit@gmail.com>
2022-12-31 15:41:29 +0200

    working version of menu and command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
-use std::{io, net::IpAddr};
+use std::{io::Write, net::IpAddr};
+use std::io;
 use public_ip;
 use tokio;
 
@@ -7,49 +8,56 @@ use tokio;
 #[command(author, version, about, long_about = None)]
 struct Args {
     #[arg(short, long, default_value_t = String::from("Empty"))]
-    target_nat_ip: String,
+    remote_nat_ip: String,
 }
 
-async fn print_external_ip() -> Option<IpAddr> {
+async fn get_nat_ip() -> Option<IpAddr> {
     // Attempt to get an IP address and print it.
     public_ip::addr().await
+}
+
+fn get_remote_nat_ip_from_user() -> IpAddr {
+    io::stdout().flush().expect("Failed to flush stdout");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to readline");
+    input.pop();
+    input.parse::<IpAddr>().expect("Invalid remote NAT IP")
 }
 
 #[tokio::main]
 async fn main() -> io::Result<()>{
     let args = Args::parse();
     let mut unused = String::new();
-
-    let external_ip = print_external_ip().await;
-    let external_ip = match external_ip {
-        Some(x) => x,
-        None => panic!("Failed to get your external IP :(")
-    };
     
+    let nat_ip = get_nat_ip().await.expect("Failed to get your external IP :(");
     println!("Welcome to NAT Punching library!");
-    println!("Both you and your peer should get each other's external IPs");
-    println!("Your external IP: {:?} (tell your peer :))", external_ip);
-    let target_nat_ip = match args.target_nat_ip.as_str() {
+    println!("-------------------------------------");
+    println!("Your external IP: {:?}", nat_ip);
+    println!("On your remote machine run: nat_punching --remote-nat-ip {:?}", nat_ip);
+    println!("");
+    
+    let remote_nat_ip = match args.remote_nat_ip.as_str() {
         "Empty" => {
-            println!("Please enter your peer's external IP:");
-            let mut target_nat_ip_input = String::new();
-            io::stdin().read_line(&mut target_nat_ip_input).expect("failed to readline...");
-            //let target_nat_ip_input = tokio::io::read_to_string(io::stdin())?;
-            println!("entered: {:?}", target_nat_ip_input);
-            target_nat_ip_input.as_str().parse::<IpAddr>().expect("Invalid target NAT IP")
+            /* Did not pass remote-nat-ip in command line so we ask here to provide it */
+            print!("Enter your remote NAT IP> ");
+            get_remote_nat_ip_from_user()
         },
-        _ => args.target_nat_ip.parse::<IpAddr>().expect("Invalid target NAT IP")
+        _ => {
+            /* Parse the command line argument */
+            args.remote_nat_ip.parse::<IpAddr>().expect("Invalid remote NAT IP")
+        }
     };
     
     println!("");
-    println!("Connection information:");
-    println!("[Your NAT IP ({:?})] <--> [ Target NAT IP ({:?})]", external_ip, target_nat_ip);
-
+    println!("Connection information");
+    println!("--------------------------");
+    println!("[{:?} (this machine)] <--> [{:?} (remote machine)]", nat_ip, remote_nat_ip);
     println!("");
-    println!("On your target machine run: nat_punching --target-nat-ip {:?}", external_ip);
-    println!("press <ENTER> to continue to establish connection...");
-    io::stdin().read_line(&mut unused).expect("failed to readline...");
-    println!("Trying to punch {:?}...", target_nat_ip);
+    
+    println!("press <ENTER> to connect...");
+    io::stdin().read_line(&mut unused).expect("Failed to readline");
+
+    println!("Trying to punch...");
 
     Ok(())
 }
