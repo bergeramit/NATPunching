b49f87f26b635537d003e576a848d88f314bc7ec
bergeramit <bergeramit@gmail.com>
2023-01-02 18:13:53 +0200

    moved retreiving public ip to UdpHoleEndpoint

--- src/lib.rs
+++ src/lib.rs
@@ -2,32 +2,39 @@ pub mod nat_punch {
     use std::net;
     use std::fmt;
     use std::io;
+    use public_ip;
 
-    pub struct Connection {
-        pub local_nat_ip: net::IpAddr,
-        pub remote_nat_ip: net::IpAddr,
+    #[derive(Default)]
+    pub struct UdpHoleEndpoint {
+        pub local_nat_ip: Option<net::IpAddr>,
+        pub remote_nat_ip: Option<net::IpAddr>,
         pub remote_nat_port: i32,
         pub local_port: i32,
     }
 
-    impl Connection {
+    impl UdpHoleEndpoint {
+        pub async fn create() -> Self {
+            let local_nat_ip = public_ip::addr().await.expect("Failed to get your external IP :(");
+            Self{local_nat_ip: Some(local_nat_ip), ..Default::default()}
+        }
+
         pub fn connect(&self) -> io::Result<()> {
             println!("Connected!");
             Ok(())
         }
     }
 
-    impl fmt::Display for Connection {
+    impl fmt::Display for UdpHoleEndpoint {
         fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
             writeln!(f,"").unwrap();
-            writeln!(f,"Connection information").unwrap();
+            writeln!(f,"UdpHoleEndpoint information").unwrap();
             writeln!(f,"--------------------------").unwrap();
             writeln!(
                 f, 
                 "[{:?}:{:?} (this machine)] <--> [{:?}:{:?} (remote machine)]",
-                self.local_nat_ip,
+                self.local_nat_ip.unwrap(),
                 self.local_port,
-                self.remote_nat_ip,
+                self.remote_nat_ip.unwrap(),
                 self.remote_nat_port
             )
         }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,20 @@
 use clap::Parser;
-use std::{io::Write, net::IpAddr};
-use std::io;
+use std::{io::{self, Write}, net::IpAddr};
 use tokio;
-use public_ip;
-use nat_punching::nat_punch::{self};
+use nat_punching::nat_punch;
 
 #[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
+#[command(author="Amit Berger", version, about)]
+/// very simple P2P connection using UDP NAT puching
 struct Args {
-    #[arg(short, long, default_value_t = String::from("Empty"))]
-    remote_nat_ip: String,
+    #[arg(short('i'), long)]
+    remote_nat_ip: Option<String>,
 
-    #[arg(short('p'), long, default_value_t = 0)]
-    remote_nat_port: i32,
+    #[arg(short('p'), long)]
+    remote_nat_port: Option<i32>,
 
-    #[arg(short('l'), long, default_value_t = 0)]
-    local_port: i32,
+    #[arg(short('l'), long)]
+    local_port: Option<i32>,
 }
 
 #[allow(unused_macros)]
@@ -29,36 +28,36 @@ macro_rules! read {
 }
 
 fn get_cmd_args(args: Args) -> (IpAddr, i32, i32) {
-    let remote_nat_ip = match args.remote_nat_ip.as_str() {
-        "Empty" => {
+    let remote_nat_ip = match args.remote_nat_ip {
+        None => {
             /* Did not pass remote-nat-ip in command line so we ask here to provide it */
             print!("Enter remote NAT IP> ");
             read!(x as IpAddr);
             x
         },
-        _ => {
+        Some(x) => {
             /* Parse the command line argument */
-            args.remote_nat_ip.parse::<IpAddr>().expect("Invalid remote NAT IP")
+            x.parse::<IpAddr>().expect("Invalid remote NAT IP")
         }
     };
     let remote_nat_port = match args.remote_nat_port {
-        0 => {
+        None => {
             /* Did not pass remote-nat-port in command line so we ask here to provide it */
             print!("Enter remote NAT PORT> ");
             read!(x as i32);
             x
         }
-        _ => { args.remote_nat_port }
+        Some(x) => x
     };
 
     let local_port = match args.local_port {
-        0 => {
+        None => {
             /* Did not pass remote-nat-port in command line so we ask here to provide it */
             print!("Enter local PORT> ");
             read!(x as i32);
             x
         }
-        _ => { args.local_port }
+        Some(x) => x
     };
     (remote_nat_ip, remote_nat_port, local_port)
 }
@@ -68,24 +67,32 @@ async fn main() -> io::Result<()>{
     let args = Args::parse();
     let mut unused = String::new();
     
-    let local_nat_ip = public_ip::addr().await.expect("Failed to get your external IP :(");
-    println!("Welcome to NAT Punching library! (local NAT IP: {:?})", local_nat_ip);
+    //let local_nat_ip = public_ip::addr().await.expect("Failed to get your external IP :(");
+    let mut endpoint = nat_punch::UdpHoleEndpoint::create().await;
+    println!("Welcome to NAT Punching library! (local NAT IP: {:?})", endpoint.local_nat_ip);
     println!("-------------------------------------------------------------");
     println!();
     
     let (remote_nat_ip, remote_nat_port, local_port) = get_cmd_args(args);
-    let conn = nat_punch::Connection{local_nat_ip, remote_nat_ip, remote_nat_port, local_port};
-    println!("{conn}");
+    endpoint.remote_nat_ip =  Some(remote_nat_ip);
+    endpoint.remote_nat_port =  remote_nat_port;
+    endpoint.local_port =  local_port;
+    println!("{endpoint}");
 
     println!("On remote machine run:");
-    println!("nat_punching --remote-nat-ip {local_nat_ip} --remote-nat-port {local_port} --local-port {remote_nat_port}");
+    println!(
+        "nat_punching --remote-nat-ip {} --remote-nat-port {} --local-port {}",
+        endpoint.local_nat_ip.unwrap(),
+        endpoint.local_port,
+        endpoint.remote_nat_port
+    );
     println!();
 
     println!("press <ENTER> to connect...");
     io::stdin().read_line(&mut unused).expect("Failed to readline");
     println!("Trying to punch...");
     
-    conn.connect().expect("Failed to connect");
+    endpoint.connect().expect("Failed to connect");
 
     Ok(())
 }
