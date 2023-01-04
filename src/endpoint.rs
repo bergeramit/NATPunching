96054911ed50fc525636e8a792f1927773a856dc
Amit Berger <bergeramit@gmail.com>
2023-01-04 20:03:19 +0200

    fixed constructor for endpoint

--- src/endpoint.rs
+++ src/endpoint.rs
@@ -1,8 +1,6 @@
 use std::net;
 use std::fmt;
 use std::io;
-use std::str::FromStr;
-use public_ip;
 
 #[allow(unused_macros)]
 macro_rules! validate_lock {
@@ -22,13 +20,12 @@ pub struct UdpHoleEndpoint {
 }
 
 impl UdpHoleEndpoint {
-    pub async fn create() -> Self {
-        let local_nat_ip = public_ip::addr().await.expect("Failed to get your external IP :(");
+    pub fn new(local_nat_ip: net::IpAddr, remote_nat_ip: net::IpAddr, remote_nat_port: i32, local_port: i32) -> Self {
         Self{
             local_nat_ip,
-            remote_nat_ip: net::IpAddr::from_str("0.0.0.0").unwrap(),
-            remote_nat_port: -1,
-            local_port: -1,
+            remote_nat_ip,
+            remote_nat_port,
+            local_port,
             lock_connection: false
         }
     }
@@ -45,17 +42,6 @@ impl UdpHoleEndpoint {
         self.lock_connection = false;
         println!("Disconnected!");
         Ok(())
-    }
-
-    pub fn set_local_port(&mut self, port: i32) {
-        validate_lock!(self);
-        self.local_port = port;
-    }
-
-    pub fn set_remote_address(&mut self, remote_nat_ip: net::IpAddr, remote_nat_port: i32) {
-        validate_lock!(self);
-        self.remote_nat_ip = remote_nat_ip;
-        self.remote_nat_port = remote_nat_port;
     }
 
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod endpoint;
 
 use std::io;
 use endpoint::UdpHoleEndpoint;
+use public_ip;
 use tokio;
 
 fn wait_for_enter() {
@@ -14,15 +15,13 @@ fn wait_for_enter() {
 #[tokio::main]
 async fn main() -> io::Result<()>{
     
-    let mut endpoint = UdpHoleEndpoint::create().await;
-    println!("Welcome to NAT Punching library! (local NAT IP: {:?})", endpoint.local_nat_ip);
+    let local_nat_ip = public_ip::addr().await.expect("Failed to get your external IP :(");
+    println!("Welcome to NAT Punching library! (local NAT IP: {:?})", local_nat_ip);
     println!("-------------------------------------------------------------");
     println!();
 
     let (remote_nat_ip, remote_nat_port, local_port) = args::parse_with_input_fill();
-    endpoint.set_local_port(local_port);
-    endpoint.set_remote_address(remote_nat_ip, remote_nat_port);
-    
+    let mut endpoint = UdpHoleEndpoint::new(local_nat_ip, remote_nat_ip, remote_nat_port, local_port);
 
     println!("{endpoint}");
     wait_for_enter();
